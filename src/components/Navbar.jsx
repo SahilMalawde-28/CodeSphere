f69cbb2e23f106a61c3330f45e8e4da03eb45749
sahilmalawde <msahil2806@gmail.com>
2024-08-24 10:30:56 +0530

    SECOND commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='w-full bg-black text-slate-400 h-[15vh] flex  items-center px-10'>
       <h2 className="text-center text-4xl font-semibold font-[poppins] text-slate-400">
-        {"<HAcKer'$ PoRtaL/>".split("").map((child, idx) => (
+        {"EventSphere".split("").map((child, idx) => (
           <span className='transition-all hover:text-white hover:font-bold' key={idx}>
             {child}
           </span>
